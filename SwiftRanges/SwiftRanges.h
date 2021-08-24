28d010039ddad1e3a8a1f7673dc3e0fbd3292937
Masahiko Noritake <m.noritake@soleilsystems.jp>
2021-08-24 16:45:18 +0900

    add files for XCodeProject and update .gitignore.

---
+++ SwiftRanges/SwiftRanges.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftRanges.
+FOUNDATION_EXPORT double SwiftRangesVersionNumber;
+
+//! Project version string for SwiftRanges.
+FOUNDATION_EXPORT const unsigned char SwiftRangesVersionString[];
